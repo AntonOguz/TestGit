82917c9365b4dad96dc0fd91040253972723e42f
AntonOguz <antonoguz@gmail.com>
2022-06-03 00:56:13 +0300

    Added Math round new

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function hello() {
     console.log('hello');
 }
 function generateNumber() {
-    const num = Math.round(random()*100)
+    const num = Math.round(Math.random()*100)
     document.getElementById('text').textContent = num;
 
 }
